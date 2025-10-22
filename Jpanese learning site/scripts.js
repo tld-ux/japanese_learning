17b0e4acdc5d98fc6deb86dde88d4733d8b371e7
tld-ux <leotheghost91@gmail.com>
2025-10-22 03:22:59 -0400

    changing stuff around

--- Jpanese learning site/scripts.js
+++ Jpanese learning site/scripts.js
@@ -7,10 +7,10 @@ function toggleBackground() {
 
   if (bgToggle) {
     document.body.style.backgroundImage = "url('images/lightmodebg.jpg')";
-    document.getElementById("mode").innerHTML = "darkmode";
+    document.getElementById("mode").innerHTML = "light mode";
   } else {
     document.body.style.backgroundImage = "url('images/darkmodebg.jpg')";
-    document.getElementById("mode").innerHTML = "lightmode";
+    document.getElementById("mode").innerHTML = "dark mode";
   }
 
   // Keep background behavior consistent
