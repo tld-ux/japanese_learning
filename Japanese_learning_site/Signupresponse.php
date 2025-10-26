c99be9ddc99d194c395ceb500285b69a722d64a3
tld-ux <leotheghost91@gmail.com>
2025-10-26 06:44:39 -0400

    Update Signupresponse.php

--- Japanese_learning_site/Signupresponse.php
+++ Japanese_learning_site/Signupresponse.php
@@ -23,10 +23,10 @@
     $passwd = trim($_POST["pass"]);
     $phone = trim($_POST["phone"]);
     $email = trim($_POST["email"]);
-    $hash_pass = password_hash($pass, PASSWORD_DEFAULT);
+    $hash_pass = password_hash($passwd, PASSWORD_DEFAULT);
 
    
-    $sql = "INSERT INTO Users (username, passwd, phone, email) VALUES ('$usernm', '$hash_pass', '$phone', '$email')";
+    $sql = "INSERT INTO Users (username, passwords, phone, email) VALUES ('$usernm', '$hash_pass', '$phone', '$email')";
 
     if (mysqli_query($conn, $sql)) {
         echo "Sign up successful!</h2>";
