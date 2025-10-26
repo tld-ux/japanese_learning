16e830e73e2f3866bb6a3fadcd1409f8770a8bb0
tld-ux <leotheghost91@gmail.com>
2025-10-26 06:36:30 -0400

    tired

--- Japanese_learning_site/Signupresponse.php
+++ Japanese_learning_site/Signupresponse.php
@@ -8,29 +8,29 @@
 <main class="page">
 
 <?php
-    $username = htmlspecialchars($_GET["username"]);
-    $password = htmlspecialchars($_GET["passwd"]);
-    $phone    = htmlspecialchars($_GET["phone"]);
-    $email    = htmlspecialchars($_GET["email"]);
-
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
     $server = "localhost";
-    $username_db = "tt";
-    $password_db = "Rep0Nuk3";
-    $database = "Credentials";
+    $user_db = "tt";
+    $pass_db = "Rep0Nuk3";
+    $db = "Credentials";
 
-    $conn = mysqli_connect($server, $username_db, $password_db, $database);
-
+    $conn = mysqli_connect($server, $user_db, $pass_db, $db);
+    
     if (!$conn) {
         die("<h1> Connection failed </h1>");
     }
 
-    $sql = "INSERT INTO Users (username, passwd, phone, email) VALUES ('$username', '$hashed_password', '$phone', '$email')";
+    $usernm = trim($_POST["user"]);
+    $passwd = trim($_POST["pass"]);
+    $phone = trim($_POST["phone"]);
+    $email = trim($_POST["email"]);
+    $hash_pass = password_hash($pass, PASSWORD_DEFAULT);
+
+   
+    $sql = "INSERT INTO Users (username, passwd, phone, email) VALUES ('$usernm', '$hash_pass', '$phone', '$email')";
 
     if (mysqli_query($conn, $sql)) {
-        echo "<h2> Sign up successful!</h2>";
-        echo "<p>Welcome, $username to the Japanese learner's community.</p>";
+        echo "Sign up successful!</h2>";
+        echo "<p>Welcome, $usernm to the Japanese learners community.</p>";
     } else {
         echo "<h1> Error: Sorry, Try again later. </h2>";
     }
